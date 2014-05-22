57ae34a99be312145fef1eea1fc96498b9c6d47f
Jeferson Oliveira <jefersondeoliveiralopes@gmail.com>
2014-05-22 07:35:58 -0300

    [JAVAWEB-MOVIEDELIVERY] Template alterado

---
+++ MovieDelivery-war/web/js/func/funcs.js
@@ -0,0 +1,20 @@
+$(function(){
+	/*
+	$('.jcarousel').jcarouselAutoscroll({
+            autostart: true;
+    });
+*/
+
+    $('.box, .box-lista').hover(function(){
+			$(this).find('.box-detalhes').stop(false,true).show()
+		}, function(){
+			$(this).find('.box-detalhes').stop(false,true).hide()
+	});
+	$('#menu-content li').hover(function(){
+			$(this).find('.submenu').stop(false,true).show()
+		}, function(){
+			$(this).find('.submenu').stop(false,true).hide()
+	});
+
+
+})
