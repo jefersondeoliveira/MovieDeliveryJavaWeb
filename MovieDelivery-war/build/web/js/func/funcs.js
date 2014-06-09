ffa61453ede683d5b8fa8cab1bed3ef7b97c9cf8
Jeferson Oliveira <jefersondeoliveiralopes@gmail.com>
2014-06-09 12:24:48 -0300

    no message

---
+++ MovieDelivery-war/build/web/js/func/funcs.js
@@ -0,0 +1,21 @@
+$(function(){
+	/*
+	$('.jcarousel').jcarouselAutoscroll({
+            autostart: true;
+    });
+*/
+
+        $('.box, .box-lista').hover(function(){
+			$(this).find('.box-detalhes').stop(false,true).show()
+		}, function(){
+			$(this).find('.box-detalhes').stop(false,true).hide()
+	});
+	$('#menu-content li').hover(function(){
+			$(this).find('.submenu').stop(false,true).show()
+		}, function(){
+			$(this).find('.submenu').stop(false,true).hide()
+	});
+        
+        
+
+})
